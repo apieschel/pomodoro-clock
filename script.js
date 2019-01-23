372a158a914f4d14a2d32c28cd869bafdd3cd83c
Glitch (hello-webpage) <none>
2019-01-23 20:45:38 +0000

    🏨🔮 Checkpoint ./script.js:2265073/2870

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ let sessionTime = document.getElementById("session-length");
     });
   }
 
-  reset.addEventListener("click", resetTime);
 	document.getElementById("session-increment").addEventListener("click", increment);
 	document.getElementById("session-decrement").addEventListener("click", decrement);
 	document.getElementById("break-increment").addEventListener("click", incrementBreak);
@@ -29,11 +28,8 @@ let sessionTime = document.getElementById("session-length");
 		let new_element = old_element.cloneNode(true);
     let old_start = document.getElementById("start_stop");
 		let new_start = old_start.cloneNode(true);
-    let old_reset = document.getElementById("reset");
-		let new_reset = old_reset.cloneNode(true);
 		old_element.parentNode.replaceChild(new_element, old_element);
     old_start.parentNode.replaceChild(new_start, old_start);
-    old_reset.parentNode.replaceChild(new_reset, old_reset);
 
 		paused = false;
 
@@ -158,16 +154,81 @@ let sessionTime = document.getElementById("session-length");
 				clearInterval(x);
 			} 
 		});
+    
+    document.getElementById("start_stop").addEventListener("click", function() {
 
-	}
+			if(paused){ 
+				let time = document.getElementById("time-left").innerText; 
 
-  function resetTime() {
-			clock.innerText = "25:00";
+				if(time.length > 2) {
+					let seconds = parseInt(time.slice(-2));
+					let minutes = parseInt(time.substr(0, time.indexOf(':')));
+					countDownDate = new Date().getTime() + minutes*60000 + seconds*1000;
+				} else {
+					let minutes = parseInt(time);
+					countDownDate = new Date().getTime() + minutes*60000;
+				}
+
+				paused=false; 
+				x = setInterval(function () {
+					
+						
+			// Get todays date and time
+			let now = new Date().getTime();
+
+			// Find the distance between now and the count down date
+			let distance = countDownDate - now;
+			//console.log(1500000 - distance);
+
+			let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			let seconds = Math.round((distance % (1000 * 60)) / 1000);
+
+			if(seconds === 60) {
+				seconds = 0;
+				minutes = minutes + 1;
+			}
+
+			if(seconds < 10) {
+				seconds = "0" + seconds.toString();
+			}
+
+			// Display the result in the element with id="time-left"
+			document.getElementById("time-left").innerHTML = minutes + ":" + seconds;
+			document.title = "Pomodoro: " + minutes + ":" + seconds;
+
+			// If the count down is finished, write some text
+			if (distance < 0) {
+				clearInterval(x);
+				document.getElementById("time-left").innerHTML = "0:00";
+
+				if(isBreak) {
+					alert("Break's over! Let's get back to work.");
+					isBreak = false;
+				} else {
+					alert("Don't push yourself too hard! Time for a break.");
+					isBreak = true;
+				}
+				countDown();
+			}
+					
+				}, 1000); 
+			} else { 
+				paused=true; 
+				clearInterval(x);
+			} 
+		});
+    
+    reset.addEventListener("click", function() {
+      console.log("test");
+      paused = true;
+      clearInterval(x);
+      document.getElementById("time-left").innerHTML = "25:00";
 			document.getElementById("timer-label").innerText = "Session";
       sessionTime.innerHTML = "25";
       breakTime.innerHTML = "5";
-      paused = true;
-  }
+    });
+
+	}
 
 	function increment() {
 		if(paused) {
